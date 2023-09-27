8d8dc732778e2adf8be75ea5af5d92dc97e7a0a7
raeesasirkhot@gmail.com <raeesasirkhot@gmail.com>
2023-09-27 13:49:39 +0200

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function ValidateEmail(mail) 
-{
- if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
-  {
-    return (true)
-  }
-    alert("You have entered an invalid email address!")
-    return (false)
-}
-
+var emailField = document.getElementById("email-field")
+var emailError = document.getElementById("email-error")
 
