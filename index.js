c9f53300d70c776e29f443991f98847ebcd1c663
raeesasirkhot@gmail.com <raeesasirkhot@gmail.com>
2023-09-28 17:48:39 +0200

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-var emailField = document.getElementById("email-field")
-var emailError = document.getElementById("email-error")
+const form = document.getElementById('form');
+const placeholder = document.getElementById ('placeholder');
+
+form.addEventListener ('submit', (e) => {
+e.preventDefault();
+
+checkInputs();
+
+function checkInputs() {
+  
+  const placeholderValue = placeholder.value.trim();
+}
+});
 
