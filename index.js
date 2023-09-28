fd94336b825b7ae09e1d46a513628ea06bcf1b69
raeesasirkhot@gmail.com <raeesasirkhot@gmail.com>
2023-09-28 18:16:47 +0200

    first commit

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ checkInputs();
 function checkInputs() {
   
   const placeholderValue = placeholder.value.trim();
+  if (placeholderValue === ''){
+    setErrorFor(placeholder, 'placeholder cannot be blank')
+} else{setSuccessFor(placeholder);
+  
 }
-});
+};
 
